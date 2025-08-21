1d99f6992b532c2120dd20ac69d44b4676a974b6
Lior <Zwananiluyanda@gmail.com>
2025-08-21 23:29:06 +0200

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,25 +2,14 @@ import React from 'react';
 
 const Footer: React.FC = () => (
   <footer className="footer offset-left">
-    <div className="footer-row">
-      <img
-        src="/cup.png"
-        alt="Cupcake"
-        className="footer-image"
-      />
+    <img src="/cup.png" alt="Cupcake" className="footer-image" />
+<a href="#" aria-label="Facebook">
+  <img src="/facebook.png" alt="Facebook" />
+</a>
+<a href="#" aria-label="Twitter/X">
+  <img src="/x.png" alt="X/Twitter" />
+</a>
 
-      <div className="follow-container">
-        <p className="follow-text">Follow Us</p>
-        <div className="social-icons">
-          <a href="#" aria-label="Facebook">
-            <img src="/facebook.png" alt="Facebook" />
-          </a>
-          <a href="#" aria-label="Twitter/X">
-            <img src="/x.png" alt="X/Twitter" />
-          </a>
-        </div>
-      </div>
-    </div>
   </footer>
 );
 
